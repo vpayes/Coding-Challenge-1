fe250cfc4d891e5ef6ba83e0d31ce0603f48a858
vpayes <vanesa_payes@outlook.com>
2024-09-01 19:11:12 -0400

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,9 @@
+// Task 1: Variables and Data Types 
+
+let employeeName = "Devonte Lykins";
+const employeeID = 1203;
+var isActive = true;
+
+console.log(employeeName, typeof(employeeName));
+console.log(employeeID, typeof(employeeID));
+console.log(isActive, typeof(isActive));
