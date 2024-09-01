9153c2fa30726649ba918d996290f0dbdd33b825
vpayes <vanesa_payes@outlook.com>
2024-09-01 19:29:40 -0400

    Task 2 - Product Inventory Management

--- cc_1.js
+++ cc_1.js
@@ -7,3 +7,17 @@ var isActive = true;
 console.log(employeeName, typeof(employeeName));
 console.log(employeeID, typeof(employeeID));
 console.log(isActive, typeof(isActive));
+
+
+
+ // Task 2: Compound Data Types
+
+ let products = ["Basket Ball", "Sneakers", "Hoop"];
+ const productDetails = {
+         name: products,
+         price: 100.00,
+         inStock: 5000, 
+ }
+
+ console.log(products, productDetails)
+ 
